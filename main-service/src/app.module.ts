f1509ccd3fdbf9cc377e8b44d71f13e6c100e191
OneHedgehog <aperteda@gmail.com>
2020-04-23 21:36:50 +0300

    added redis cache

--- main-service/src/app.module.ts
+++ main-service/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {CacheInterceptor, CacheModule, Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {APP_INTERCEPTOR} from "@nestjs/core";
+import * as redisStore from 'cache-manager-redis-store';
+import {HttpCacheInterceptor} from "./interceptors/http-cache.interceptor";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        CacheModule.register({
+            store: redisStore,
+            host: 'redis-main-service',
+            port: 6379,
+        })
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        HttpCacheInterceptor,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CacheInterceptor,
+        },
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
